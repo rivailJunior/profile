f5da3b35bf5b963759fe4bde8ce20eed5b7f61b4
rivailJunior <rivail.rj@gmail.com>
2023-01-31 11:28:22 -0400

    delete: domain config is not necessary to internationalization

--- next.config.mjs
+++ next.config.mjs
@@ -13,16 +13,6 @@ const config = {
   i18n: {
     locales: ["en", "pt"],
     defaultLocale: "pt",
-    domains: [
-      {
-        domain: "/en",
-        defaultLocale: "en",
-      },
-      {
-        domain: "/pt",
-        defaultLocale: "pt",
-      },
-    ],
   },
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
