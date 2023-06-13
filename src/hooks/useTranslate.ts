fe14bb871fd52a5fc473198b55617b3acd1fe576
rivailJunior <rivail.rj@gmail.com>
2023-06-12 22:06:39 -0400

    fix: remove unnecessary code

--- src/hooks/useTranslate.ts
+++ src/hooks/useTranslate.ts
@@ -1,17 +1,6 @@
-"use client"
-import { useRouter } from "next/navigation";
-// import { getTranslation } from "../helper/i18n-helper";
-
 export default function useTranslate(locale: string) {
-  const router = useRouter();
-  // const { locale } = router;
-
-  /*TODO: inspect this function*/
-  const changeLanguage = async (language: string) => {
-    await router.push("/", "/", { locale: language });
-  };
 
   const isBR = locale === "pt";
 
-  return { changeLanguage, isBR };
+  return { isBR };
 }
